f302dde7db75252c8377725a3e83134361282375
opilane <opilane@HP-CZC6297B8P>
2021-02-18 10:51:21 +0200

    almost done with this codiii

--- Menu_APP/Menu_APP/MainPage.xaml.cs
+++ Menu_APP/Menu_APP/MainPage.xaml.cs
@@ -12,7 +12,7 @@ namespace Menu_APP
 {
     public partial class MainPage : MasterDetailPage
     {
-        
+        public string result = "7041";
        
         public MainPage()
         {
@@ -47,70 +47,89 @@ namespace Menu_APP
 
             });
 
+            while(result == "7041")
 
-            list.Add(new MasterMenuitems()
             {
+
+
                 
-                Text = "О Банковской системе",
-                Detail = "дополнительная безопастность",
-                ImagePath = "lok.png",
-                TargetPage = typeof(Views.onePage)
-            });
+                
+                    list.Add(new MasterMenuitems()
+                    {
+
+                        Text = "О Банковской системе",
+                        Detail = "дополнительная безопастность",
+                        ImagePath = "lok.png",
+                        TargetPage = typeof(Views.onePage)
+                    });
+                
+                //onePage
 
 
-            list.Add(new MasterMenuitems()
-            {
-                Text = "Дополнительная информация",
-                Detail = "Часто задаваемые вопросы",
-                ImagePath = "qu.png",
-                TargetPage = typeof(Views.twoPage)
-            });
+                    list.Add(new MasterMenuitems()
+                    {
+                        Text = "Дополнительная информация",
+                        Detail = "Часто задаваемые вопросы",
+                        ImagePath = "qu.png",
+                        TargetPage = typeof(Views.twoPage)
+                    });
+                
 
 
-            list.Add(new MasterMenuitems()
-            {
-                Text = "Обзор",
-                ImagePath = "hos.png",
-                TargetPage = typeof(Views.threePage)
-            });
 
-            list.Add(new MasterMenuitems()
-            {
-                Text = "Платежи",
-               
-                ImagePath = "mon.png",
-                TargetPage = typeof(Views.fourPage)
-            });
+                    list.Add(new MasterMenuitems()
+                    {
+                        Text = "Обзор",
+                        ImagePath = "hos.png",
+                        TargetPage = typeof(Views.threePage)
+                    });
+                
 
-            list.Add(new MasterMenuitems()
-            {
-                Text = "Карты",
-                Detail = "выбор банковских карт",
-                ImagePath = "card.png",
-                TargetPage = typeof(Views.fivePage)
-            });
 
-            list.Add(new MasterMenuitems()
-            {
-                Text = "Услуги",
-                ImagePath = "list.png",
-                TargetPage = typeof(Views.sixPage)
-            });
 
-            list.Add(new MasterMenuitems()
-            {
-                Text = "Контакты",
-                Detail = "Частный клиент",
-                ImagePath = "kont.png",
-                TargetPage = typeof(Views.sevenPage)
-            });
+                    list.Add(new MasterMenuitems()
+                    {
+                        Text = "Платежи",
 
-            list.Add(new MasterMenuitems()
-            {
-                Text = "Выход",
-                ImagePath = "kont1.png",
-                TargetPage = typeof(Views.eightPage)
-            });
+                        ImagePath = "mon.png",
+                        TargetPage = typeof(Views.fourPage)
+                    });
+                
+
+                list.Add(new MasterMenuitems()
+                {
+                    Text = "Карты",
+                    Detail = "выбор банковских карт",
+                    ImagePath = "card.png",
+                    TargetPage = typeof(Views.fivePage)
+                });
+
+                list.Add(new MasterMenuitems()
+                {
+                    Text = "Услуги",
+                    ImagePath = "list.png",
+                    TargetPage = typeof(Views.sixPage)
+                });
+
+                list.Add(new MasterMenuitems()
+                {
+                    Text = "Контакты",
+                    Detail = "Частный клиент",
+                    ImagePath = "kont.png",
+                    TargetPage = typeof(Views.sevenPage)
+                });
+
+                list.Add(new MasterMenuitems()
+                {
+                    Text = "Выход",
+                    ImagePath = "kont1.png",
+                    TargetPage = typeof(Views.eightPage)
+                });
+                break;   
+            }
+            
+            
+            
             return list;
         }
 
--- Menu_APP/Menu_APP/Views/AboutPage.xaml.cs
+++ Menu_APP/Menu_APP/Views/AboutPage.xaml.cs
@@ -14,18 +14,30 @@ namespace Menu_APP.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AboutPage : ContentPage
     {
+        public string result = "0";
         public AboutPage()
         {
             InitializeComponent();
         }
 
+        //public async void OnActionSheetSimple1_Clicked(object sender, EventArgs e)
+        //{
+            //string result1 = await DisplayPromptAsync("Enter your smart ID", "password", initialValue: "result", maxLength: 4, keyboard: Keyboard.Numeric);
+            
+        //}
+
         public async void OnActionSheetSimple_Clicked(object sender, EventArgs e)
         {
-            string result = await DisplayPromptAsync("Enter your smart ID", "password", maxLength: 4, keyboard: Keyboard.Numeric);
-            if (result == "7041")
+            
+            string result1 = await DisplayPromptAsync("Enter your smart ID", "password", maxLength: 4, keyboard: Keyboard.Numeric);
+            if (result1 == "7041")
             {
-                
-                //await DisplayAlert("Alert", "You have been alerted", "OK");
+                result = "7041";
+                await DisplayAlert("Alert", "Access gained", "OK");
+            }
+            else
+            {
+                await DisplayAlert("Alert", "Smart-ID code is incorrect", "OK");
             }
         }
 
