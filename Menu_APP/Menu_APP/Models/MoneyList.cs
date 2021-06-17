ca48a49f2db9fd7f427b571c69d0c533498aacb5
opilane <opilane@HP-CZC6297B89>
2021-06-17 13:08:03 +0300

    MVVM edit

---
+++ Menu_APP/Menu_APP/Models/MoneyList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Menu_APP.Models
+{
+    class MoneyList
+    {
+        public string AllMoney { get; set; }
+        public string SplitMoney { get; set; }
+        public string HowMoney { get; set; }
+    }
+}
