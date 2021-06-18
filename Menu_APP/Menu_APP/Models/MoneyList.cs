ad49f22a3f3efecc6f6a4cfe3e94c2fd05f0d1a7
opilane <opilane@HP-CZC6297B89>
2021-06-18 09:45:53 +0300

    sql connection

--- Menu_APP/Menu_APP/Models/MoneyList.cs
+++ Menu_APP/Menu_APP/Models/MoneyList.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using SQLite;
 
 namespace Menu_APP.Models
 {
-    class MoneyList
+    [Table("MoneyList")]
+    public class MoneyList
     {
+        [PrimaryKey, AutoIncrement, Column("_id")]
+        public int Id { get; set; }
+
         public string AllMoney { get; set; }
         public string SplitMoney { get; set; }
         public string HowMoney { get; set; }
