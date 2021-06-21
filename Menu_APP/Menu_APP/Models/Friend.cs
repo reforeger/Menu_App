5357a10ae219a1fdbe21d9072520b00d3b96ca7b
opilane <opilane@HP-CZC6297B89>
2021-06-21 13:29:47 +0300

    MVVM

---
+++ Menu_APP/Menu_APP/Models/Friend.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Menu_APP.Models
+{
+    public class Friend
+    {
+
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+    }
+}
